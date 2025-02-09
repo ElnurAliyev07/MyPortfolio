c491d0d772b77125b8bb5228077c4b679c6e5011
ElnurAliyev07 <Eliyev.Elnur@ascca.edu.az>
2025-02-09 15:46:23 +0400

    third commit

--- MyPortfolio/settings.py
+++ MyPortfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure--fm8_2l)ijjz%ao21z@6@+6ijlh_crj+m^7w8pp+yu%j+ub$dw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['myportfolio-8ide.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
